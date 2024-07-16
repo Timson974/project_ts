c502a5613ecada7e220120a297cb644948451ec1
Timson974 <t.utepov@hotmail.com>
2024-07-16 12:10:13 +0100

    Add step 3. Correct sidebar.js

--- frontend/src/components/sidebar.js
+++ frontend/src/components/sidebar.js
@@ -1,7 +1,8 @@
-// step2
+import {Tooltip} from 'bootstrap';
 import {Auth} from "../services/auth.js";
 import {CustomHttp} from "../services/custom-http.js";
 import config from "../../config/config.js";
+import {UpdateBalance} from "../utils/updateBalance.js";
 
 export class Sidebar {
     _sidebarTemplate = '../templates/sidebar.html';
@@ -14,15 +15,25 @@ export class Sidebar {
         this.navLinksElements = null;
         this.headerBtnMenuButtonElement = null;
         this.menuMobileElement = null;
+        this.parentBalanceElement = null;
         this.balance = null;
         this.balanceElement = null;
+        this.editBalanceInput = null;
+        this.wrapBalanceElement = null;
         this.fullNameElement = null;
+        this.editBalanceInputElement = null;
+        this.saveBalanceButtonElement = null;
+        this.canselBalanceButtonElement = null;
+
 
         this.init()
     }
 
     async init() {
-
+        const accessTokenKey = localStorage.getItem(Auth.accessTokenKey);
+        if (!accessTokenKey) {
+            return window.location.href = '#/login';
+        }
         try {
             const result = await CustomHttp.request(config.host + '/balance');
             if (result) {
@@ -30,7 +41,7 @@ export class Sidebar {
                     throw new Error(result.error);
                 }
                 this.balance = result.balance;
-                this.drawSidebar()
+                this.drawSidebar();
 
             }
         } catch (e) {
@@ -52,10 +63,16 @@ export class Sidebar {
 
         this.headerBtnMenuButtonElement = document.getElementById('header__menu-btn');
         this.menuMobileElement = document.getElementById('sidebar_wrap');
-
         this.balanceElement = document.getElementById('balance');
+        if (this.balanceElement) {
+            this.balanceElement.innerText = `${this.balance}$`;
+            this.balanceElement.addEventListener('click', function () {
+                that.editBalance();
+            })
+        }
+
+
         this.fullNameElement = document.getElementById('full-name');
-        this.balanceElement.innerText = `${this.balance}$`
         this.fullNameElement.innerText = `${userInfo.name}`;
 
         this.headerBtnMenuButtonElement.addEventListener('click', () => {
@@ -68,11 +85,11 @@ export class Sidebar {
             }
         })
 
-        if (this.urlRoute === '#/expenses'
+        if (this.urlRoute === '#/expense'
             || this.urlRoute === '#/income'
-            || this.urlRoute === '#/create-expenses'
+            || this.urlRoute === '#/create-expense'
             || this.urlRoute === '#/create-income'
-            || this.urlRoute === '#/edit-expenses'
+            || this.urlRoute === '#/edit-expense'
             || this.urlRoute === '#/edit-income'
         ) {
             this.accordionButtonElement.classList.remove('collapsed');
@@ -98,9 +115,9 @@ export class Sidebar {
                 link.classList.remove('active');
             }
 
-            if ((that.urlRoute === '#/create-expenses' && link.getAttribute('href') === '#/expenses')
-                || (that.urlRoute === '#/edit-expenses' && link.getAttribute('href') === '#/expenses')
-                || (that.urlRoute === '#/expenses' && link.getAttribute('href') === '#/expenses')) {
+            if ((that.urlRoute === '#/create-expense' && link.getAttribute('href') === '#/expense')
+                || (that.urlRoute === '#/edit-expense' && link.getAttribute('href') === '#/expense')
+                || (that.urlRoute === '#/expense' && link.getAttribute('href') === '#/expense')) {
                 link.classList.add('active');
             }
             if ((that.urlRoute === '#/create-income' && link.getAttribute('href') === '#/income')
@@ -115,6 +132,72 @@ export class Sidebar {
                 link.classList.remove('link-dark')
             }
         });
+        [...document.querySelectorAll('[data-bs-toggle="tooltip"]')]
+            .forEach(el => new Tooltip(el));
     }
 
+    async editBalance() {
+        const that = this;
+        this.wrapBalanceElement = document.getElementById('wrapBalance');
+        this.parentBalanceElement = document.getElementById('parentBalance');
+        this.editBalanceInputElement = document.createElement('input');
+        this.editBalanceInputElement.className = 'form-control';
+        this.editBalanceInputElement.setAttribute('type', 'number');
+        this.editBalanceInputElement.setAttribute('name', 'editBalanceInput');
+        this.editBalanceInputElement.setAttribute('id', 'editBalanceInput');
+        this.editBalanceInputElement.setAttribute('patern', 'editBalanceInput');
+
+        this.editBalanceInputElement.value = await UpdateBalance.getBalance();
+
+        this.saveBalanceButtonElement = document.createElement('button');
+        this.saveBalanceButtonElement.className = 'btn';
+        this.saveBalanceButtonElement.setAttribute('id', 'saveBalance');
+        const saveImageElement = document.createElement('img');
+        saveImageElement.setAttribute('src', 'images/check2.svg');
+        saveImageElement.setAttribute('alt', 'подтвердить');
+        this.saveBalanceButtonElement.appendChild(saveImageElement);
+
+        this.canselBalanceButtonElement = document.createElement('button');
+        this.canselBalanceButtonElement.className = 'btn';
+        this.canselBalanceButtonElement.setAttribute('id', 'canselEditBalance');
+        const canselImageElement = document.createElement('img');
+        canselImageElement.setAttribute('src', 'images/x.svg');
+        canselImageElement.setAttribute('alt', 'отменить');
+        this.canselBalanceButtonElement.appendChild(canselImageElement);
+        this.parentBalanceElement.style.display = 'none';
+        this.wrapBalanceElement.appendChild(this.editBalanceInputElement);
+        this.wrapBalanceElement.appendChild(this.saveBalanceButtonElement);
+        this.wrapBalanceElement.appendChild(this.canselBalanceButtonElement);
+
+        this.canselBalanceButtonElement.addEventListener('click', function () {
+            that.canselSaveBalance()
+        })
+
+        this.saveBalanceButtonElement.addEventListener('click', () => {
+            that.saveBalance();
+        })
+    }
+
+    canselSaveBalance() {
+        this.parentBalanceElement.removeAttribute('style')
+        this.editBalanceInputElement.remove();
+        this.saveBalanceButtonElement.remove();
+        this.canselBalanceButtonElement.remove();
+    }
+
+    async saveBalance() {
+        this.editBalanceInput = document.getElementById('editBalanceInput');
+        try {
+            const response = await CustomHttp.request(config.host + '/balance', 'PUT', {
+                newBalance: this.editBalanceInput.value
+            });
+            if (response && response.balance) {
+                this.balance = response.balance;
+                this.canselSaveBalance();
+                this.balanceElement.innerText = `${this.balance}$`;
+            }
+        } catch (error) {
+            throw new Error(error);
+        }
+    }
 }
